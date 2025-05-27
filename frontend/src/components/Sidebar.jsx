38ede902e73703b3d10212496c929a142de1ee0e
mrccdric27 <mayuga.marccedricc@gmail.com>
2025-05-27 20:50:12 +0800

    sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,47 +7,80 @@ export default function Sidebar() {
       <h2 className={styles.title}>Navigation</h2>
       <ul className={styles.navList}>
         <li>
-          <NavLink to="/" className={({ isActive }) => 
-            `${styles.link} ${isActive ? styles.active : ""}`} end>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.active : ""}`
+            }
+            end
+          >
             <span className={styles.icon}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M12 2L2 12h3v8h14v-8h3L12 2z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M12 2L2 12h3v8h14v-8h3L12 2z" />
               </svg>
             </span>
-            Inspire
+            <span className={styles.label}>Inspire</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/asset" className={({ isActive }) =>
-            `${styles.link} ${isActive ? styles.active : ""}`}>
+          <NavLink
+            to="/asset"
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.active : ""}`
+            }
+          >
             <span className={styles.icon}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M3 4v16h18V4H3zm16 14H5V6h14v12z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M3 4v16h18V4H3zm16 14H5V6h14v12z" />
               </svg>
             </span>
-            Asset
+            <span className={styles.label}>Asset</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/add" className={({ isActive }) =>
-            `${styles.link} ${isActive ? styles.active : ""}`}>
+          <NavLink
+            to="/add"
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.active : ""}`
+            }
+          >
             <span className={styles.icon}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M19 13H13v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19 13H13v6h-2v-6H5v-2h6V5h2v6h6v2z" />
               </svg>
             </span>
-            Create
+            <span className={styles.label}>Create</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/category" className={({ isActive }) =>
-            `${styles.link} ${isActive ? styles.active : ""}`}>
+          <NavLink
+            to="/category"
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.active : ""}`
+            }
+          >
             <span className={styles.icon}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M3 13h2v-2H3v2zm4 0h14v-2H7v2z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M3 13h2v-2H3v2zm4 0h14v-2H7v2z" />
               </svg>
             </span>
-            Category Settings
+            <span className={styles.label}>Category Settings</span>
           </NavLink>
         </li>
       </ul>
