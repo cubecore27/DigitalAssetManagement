4950a538cef6f41ec1db53a096e2713acf6cc66d
mrccdric27 <cubecore27@gmail.com>
2025-07-01 17:53:50 +0800

    Update model name to mobileone_s0 in Dockerfile and views.py

--- DAMS/search/views.py
+++ DAMS/search/views.py
@@ -27,7 +27,7 @@ class ImageIndexingView(APIView):
 
             st = Search_Setup(
                 image_list=image_list,
-                model_name='resnet18.a1_in1k',
+                model_name='mobileone_s0',
                 pretrained=True,
                 image_count=100
             )
@@ -71,7 +71,7 @@ class SimilarAssetAPIView(APIView):
 
         st = Search_Setup(
             image_list=image_list,
-            model_name='resnet18.a1_in1k',
+            model_name='mobileone_s0',
             pretrained=True,
             image_count=len(image_list)
         )
